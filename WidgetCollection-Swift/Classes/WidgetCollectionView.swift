71438f4c72766e33d4bb056bbed1f465eca7a2fc
Unknown <unknown@example.com>
2023-11-08 21:17:22 +0800

    delete&exchange

--- WidgetCollection-Swift/Classes/WidgetCollectionView.swift
+++ WidgetCollection-Swift/Classes/WidgetCollectionView.swift
@@ -16,13 +16,22 @@ public protocol WidgetCollectionViewDataSource: NSObjectProtocol {
     
 }
 
-public protocol WidgetCollectionViewDelegate: UIScrollViewDelegate {
+@objc public protocol WidgetCollectionViewDelegate: UIScrollViewDelegate {
     
     func collectionView(_ collectionView: WidgetCollectionView, didSelectItemAt index: Int)
+    @objc optional func collectionView(_ collectionView: WidgetCollectionView, didDeleteItemAt index: Int)
+    @objc optional func collectionView(_ collectionView: WidgetCollectionView, canDeleteItemAt index: Int) -> Bool
+    @objc optional func collectionView(_ collectionView: WidgetCollectionView, exchangeItemAt index: Int, toIndex: Int)
     
 }
 
-open class WidgetCollectionView: UIScrollView, UIScrollViewDelegate {
+protocol WidgetCollectionCellDelegate: NSObjectProtocol {
+    
+    func didTouchDelete(_ cell: WidgetCollectionCell, didDeleteItemAt index: Int)
+    
+}
+
+open class WidgetCollectionView: UIScrollView, UIScrollViewDelegate, WidgetCollectionCellDelegate {
     
     private var allScreens: [WidgetCollectionCell] = [],
                 outOfScreens: [String: WidgetCollectionCell] = [:],
@@ -31,7 +40,11 @@ open class WidgetCollectionView: UIScrollView, UIScrollViewDelegate {
                 minX: CGFloat = 0,
                 minY: CGFloat = 0,
                 maxY: CGFloat = 0,
-                enableSelected: Bool = true
+                enableSelected: Bool = true,
+                inShake: Bool = false,
+                lastCenter: CGPoint = .zero,
+                comparePoint: CGPoint = .zero,
+                lastCell: WidgetCollectionCell?
     
     public var spacing: CGFloat = 0
     
@@ -44,6 +57,13 @@ open class WidgetCollectionView: UIScrollView, UIScrollViewDelegate {
                 delegate = self
             }
         }
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(widgetLongGesture(_:)))
+        addGestureRecognizer(longPress)
     }
     
     public func reloadData() {
@@ -93,6 +113,7 @@ open class WidgetCollectionView: UIScrollView, UIScrollViewDelegate {
         }
         if let viewClass = registers[identifier] as? WidgetCollectionCell.Type {
             cell = viewClass.init(reuseIdentifier: identifier, index: index)
+            cell?.delegate = self
         }
         if let _ = cell {
             onScreens[reuseIdentifier] = cell
@@ -106,6 +127,7 @@ open class WidgetCollectionView: UIScrollView, UIScrollViewDelegate {
     }
     
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        stopShake()
         var screenFrame = scrollView.bounds
         screenFrame.origin = scrollView.contentOffset
         allScreens.forEach { v in
@@ -191,12 +213,138 @@ open class WidgetCollectionView: UIScrollView, UIScrollViewDelegate {
         return self
     }
     
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        stopShake()
+    }
+    
+    @objc private func widgetLongGesture(_ sender: UILongPressGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            if inShake || lastCell != nil {
+                return
+            }
+            let point = sender.location(in: sender.view)
+            for view in allScreens {
+                if view.frame.contains(point) {
+                    bringSubviewToFront(view)
+                    let p = sender.location(in: view)
+                    comparePoint = CGPoint(x: p.x-view.bounds.midX, y: p.y-view.bounds.midY)
+                    view.startShake()
+                    lastCell = view
+                    lastCenter = view.center
+                    return
+                }
+            }
+            inShake = true
+            onScreens.forEach { key, value in
+                if let shake = widgetDelegate?.collectionView?(self, canDeleteItemAt: value.index), shake {
+                    value.deleteBtn.isHidden = false
+                    value.startShake()
+                }
+            }
+        case .changed:
+            if let cell = lastCell {
+                let point = sender.location(in: sender.view)
+                cell.center = CGPoint(x: point.x-comparePoint.x, y: point.y-comparePoint.y)
+                var inOther = false
+                for view in allScreens {
+                    if view.index != cell.index, view.frame.contains(cell.center) {
+                        inOther = true
+                        break
+                    }
+                }
+                if inOther {
+                    cell.contentView.layer.shadowColor = UIColor.green.cgColor
+                    cell.contentView.layer.shadowOpacity = 0.5
+                }else {
+                    cell.contentView.layer.shadowColor = UIColor.red.cgColor
+                    cell.contentView.layer.shadowOpacity = 0.2
+                }
+            }
+        case .ended:
+            if let cell = lastCell {
+                cell.contentView.layer.shadowOpacity = 0
+                for view in allScreens {
+                    if view.index != cell.index, view.frame.contains(cell.center) {
+                        widgetDelegate?.collectionView?(self, exchangeItemAt: cell.index, toIndex: view.index)
+                        break
+                    }
+                }
+                UIView.animate(withDuration: 0.25) {
+                    cell.stopShake()
+                    cell.center = self.lastCenter
+                } completion: { finished in
+                    self.lastCell = nil
+                }
+
+            }
+        default:
+            break
+        }
+    }
+    
+    private func stopShake() {
+        if inShake {
+            inShake = false
+            allScreens.forEach { value in
+                value.stopShake()
+                value.deleteBtn.isHidden = true
+            }
+        }
+    }
+    
+    func didTouchDelete(_ cell: WidgetCollectionCell, didDeleteItemAt index: Int) {
+        widgetDelegate?.collectionView?(self, didDeleteItemAt: index)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
 open class WidgetCollectionCell: UIView {
     
-    open var reuseIdentifier: String?,
-        index: Int = 0
+    weak var delegate: WidgetCollectionCellDelegate?
+    
+    public var reuseIdentifier: String?,
+               index: Int = 0
+    
+    public lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 10
+        return view
+    }()
+    lazy var deleteBtn: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setBackgroundImage(UIImage(systemName: "minus.circle"), for: .normal)
+        button.isHidden = true
+        button.tintColor = .red
+        button.addTarget(self, action: #selector(touchDelete), for: .touchUpInside)
+        return button
+    }()
+    lazy var rotationAni: CABasicAnimation = {
+        let value = 50/180/Double.pi
+        let animation = CABasicAnimation(keyPath: "transform.rotation")
+        animation.duration = 0.15
+        animation.repeatCount = .infinity
+        animation.autoreverses = true
+        animation.fromValue = -value
+        animation.toValue = value
+        animation.isRemovedOnCompletion = false
+        return animation
+    }()
+    
+    public override var frame: CGRect {
+        didSet {
+            contentView.frame = bounds
+            deleteBtn.frame = CGRect(x: bounds.maxX-30, y: bounds.minY, width: 30, height: 30)
+        }
+    }
     
     public required convenience init(reuseIdentifier: String?, index: Int) {
         self.init()
@@ -206,7 +354,23 @@ open class WidgetCollectionCell: UIView {
     }
     
     open func setUpView() {
-        
+        addSubview(contentView)
+        addSubview(deleteBtn)
+    }
+    
+    @objc func touchDelete() {
+        delegate?.didTouchDelete(self, didDeleteItemAt: index)
+    }
+    
+    func startShake() {
+        guard let _ = contentView.layer.animation(forKey: "transform.rotation") else {
+            contentView.layer.add(rotationAni, forKey: "transform.rotation")
+            return
+        }
+    }
+    
+    func stopShake() {
+        contentView.layer.removeAllAnimations()
     }
     
 }
